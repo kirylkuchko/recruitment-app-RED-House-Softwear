c84bb344d37f0ceba6523ca9b454583ec8714531
Kiryl Kuchko <kuchkokirill@mail.ru>
2022-07-25 10:30:23 +0200

    Added function for GET requests to accuweather

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,9 +13,30 @@ function App() {
       {key:2,location:'Paris',date:'2022-07-21',temperature:'32°C'},
       {key:3,location:'Warsaw',date:'2022-07-21',temperature:'32°C'},
       {key:4,location:'Minsk',date:'2022-07-21',temperature:'32°C'},
-      {key:4,location:'Amsterdam',date:'2022-07-21',temperature:'32°C'}]
+      {key:5,location:'Amsterdam',date:'2022-07-21',temperature:'32°C'}]
     )
   }
+
+  const APIKey = 'ENSZ9FMwCghxV7dMvxl2TEr3s261wUye';
+
+  //function for accuweather request with to GET req: first for location key, second for temperature by location key
+  const request = async (location) => {
+    return fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q=${location}`)
+      .then((response) => {
+        return response.json()[0].Key;
+      }).then((locationKey) => {
+        fetch(`http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${APIKey}`)
+          .then((response) => {
+            return response.json().Temperature.Metric.Value})
+          .catch(function (e) {
+            console.log(e);
+          })
+      }).catch(function (e) {
+        console.log(e);
+      });
+  }
+
+  console.log(request('Minsk'));
 
   return (
     <div className="App">
@@ -35,6 +56,8 @@ function App() {
       </div>
     </div>
   );
+
+
 }
 
 export default App;
